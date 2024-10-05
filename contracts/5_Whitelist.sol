660ada1eb4e62348ae03155ff50f51597e12910f
JonaPlaz <jplazanet@hotmail.com>
2024-10-05 03:54:22 +0200

    [Contract Whitelist] add authorize function + receive/fallback example

--- contracts/5_Whitelist.sol
+++ contracts/5_Whitelist.sol
@@ -6,4 +6,18 @@ contract Whitelist {
     mapping (address => bool) whitelist;
 
     event Authorized(address _address);
+    // event EthReceived(address _addr, uint value);
+
+    function authorize(address _address) public {
+        whitelist[_address] = true;
+        emit Authorized(_address);
+    }
+
+    // receive() external payable {
+    //     emit EthReceived(msg.sender, msg.value);
+    //  }
+
+    // fallback() external payable {
+    //     emit EthReceived(msg.sender, msg.value);
+    //  }
 }
