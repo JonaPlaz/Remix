489189136061da15e2d99b23d5287c9eba7e723b
JonaPlaz <jplazanet@hotmail.com>
2024-10-07 16:36:24 +0200

    [Admin] add functions and use Ownable

--- contracts/9_Admin.sol
+++ contracts/9_Admin.sol
@@ -1,7 +1,42 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Admin {
-    
+contract Admin is Ownable {
+    mapping(address => bool) whitelist;
+    mapping(address => bool) blacklist;
+
+    event Whitelisted(address _address);
+    event Blacklisted(address _address);
+
+    constructor() Ownable(msg.sender) {}
+
+    modifier checkOwner() {
+        require(owner() == msg.sender, "Caller is not the owner");
+        _;
+    }
+
+    function authorize(address _address) public checkOwner {
+        require(!blacklist[_address], "already blacklist");
+        require(!whitelist[_address], "already whitelist");
+        whitelist[_address] = true;
+        emit Whitelisted(_address);
+    }
+
+    // test avec onlyOwner natif de Ownable, pas besoin de modifier
+    function unauthorize(address _address) public onlyOwner {
+        require(!blacklist[_address], "already blacklist");
+        require(!whitelist[_address], "already whitelist");
+        blacklist[_address] = true;
+        emit Blacklisted(_address);
+    }
+
+    function isWhitelisted(address _address) public view returns (bool) {
+        return whitelist[_address];
+    }
+
+    function isBlacklisted(address _address) public view returns (bool) {
+        return blacklist[_address];
+    }
 }
